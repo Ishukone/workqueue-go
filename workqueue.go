5f23f9a3bb5a395a467269b180779c4ac5f8f671
uzero <Bi Chunlei>
2015-01-04 07:20:30 +0800

    1, wrap some lock to function 2, add schedule at moment of wait goroutines free
    Signed-off-by: ishukone <uzero@colorscode.net>

--- workqueue.go
+++ workqueue.go
@@ -3,7 +3,6 @@ package workqueue
 import (
 	"runtime"
 	"sync"
-	"time"
 )
 
 type Work struct {
@@ -16,17 +15,83 @@ type WorkQueue struct {
 	mainCond     *sync.Cond
 	routinesCond *sync.Cond
 
-	routinesLock  sync.RWMutex
-	totalRoutines int
-	freeRoutines  int
+	routinesLock sync.RWMutex
+	freeRoutines int
 
 	listRWLock  sync.RWMutex
 	workList    []*Work
 	workNumbers int
 }
 
-func (wq *WorkQueue) wakeupMain() {
+func (wq *WorkQueue) waitScheduleWork() {
+	wq.mainCond.L.Lock()
+	wq.mainCond.Wait()
+	wq.mainCond.L.Unlock()
+}
+
+func (wq *WorkQueue) wakeupMainRoutine() {
+	wq.mainCond.L.Lock()
 	wq.mainCond.Signal()
+	wq.mainCond.L.Unlock()
+}
+
+func (wq *WorkQueue) waitMainSchedule() {
+	wq.routinesCond.L.Lock()
+
+	wq.routinesLock.Lock()
+	wq.freeRoutines++
+	wq.routinesLock.Unlock()
+
+	wq.routinesCond.Wait()
+
+	wq.routinesLock.Lock()
+	wq.freeRoutines--
+	wq.routinesLock.Unlock()
+
+	wq.routinesCond.L.Unlock()
+}
+
+func (wq *WorkQueue) wakeupRoutines() {
+	wq.routinesCond.L.Lock()
+	wq.routinesCond.Signal()
+	wq.routinesCond.L.Unlock()
+}
+
+func (wq *WorkQueue) fetchWork() *Work {
+	var work *Work = nil
+
+	wq.listRWLock.Lock()
+	if wq.workNumbers > 0 {
+		work = wq.workList[0]
+		if wq.workNumbers == 1 {
+			wq.workList = []*Work{}
+		} else {
+			wq.workList = wq.workList[1:]
+		}
+		wq.workNumbers--
+	}
+
+	wq.listRWLock.Unlock()
+
+	return work
+}
+
+func (wq *WorkQueue) schedulableWorks(works *int, schedule *bool) {
+	wq.listRWLock.Lock()
+	workNumbers := wq.workNumbers
+	wq.listRWLock.Unlock()
+
+	wq.routinesLock.Lock()
+	freeRoutines := wq.freeRoutines
+	wq.routinesLock.Unlock()
+
+	if freeRoutines >= workNumbers {
+		*works = workNumbers
+		*schedule = false
+	} else {
+		*works = workNumbers - freeRoutines
+		*schedule = true
+	}
 }
 
 func (wq *WorkQueue) ScheduleWork(work *Work) {
@@ -35,7 +100,7 @@ func (wq *WorkQueue) ScheduleWork(work *Work) {
 	wq.workNumbers++
 	wq.listRWLock.Unlock()
 
-	wq.wakeupMain()
+	wq.wakeupMainRoutine()
 }
 
 func CreateWorkQueue(routinesNumber int) *WorkQueue {
@@ -51,69 +116,39 @@ func CreateWorkQueue(routinesNumber int) *WorkQueue {
 		locker = new(sync.Mutex)
 		wq.routinesCond = sync.NewCond(locker)
 
+		var needSchedule bool
+		var wakeupRoutines int
 		done <- 0
 		for {
-			wq.mainCond.L.Lock()
-			wq.mainCond.Wait()
-			wq.mainCond.L.Unlock()
-
-			wq.listRWLock.Lock()
-			workNumbers := wq.workNumbers
-			wq.listRWLock.Unlock()
-
-			wq.routinesLock.Lock()
-			freeRoutines := wq.freeRoutines
-			wq.routinesLock.Unlock()
-
-			var wakeupRoutines int
-			var needSchedule bool
-			if freeRoutines > workNumbers {
-				wakeupRoutines = workNumbers
-				needSchedule = false
-			} else {
-				wakeupRoutines = workNumbers - freeRoutines
-				needSchedule = true
+			if !needSchedule {
+				wq.waitScheduleWork()
 			}
 
+			wq.schedulableWorks(&wakeupRoutines, &needSchedule)
+
 			for ; wakeupRoutines > 0; wakeupRoutines-- {
-				wq.routinesCond.L.Lock()
-				wq.routinesCond.Signal()
-				wq.routinesCond.L.Unlock()
+				wq.wakeupRoutines()
 			}
 
 			if needSchedule {
-				time.Sleep(5 * time.Second)
+				runtime.Gosched()
 			}
 		}
 	}()
 
 	<-done
 	for i := 0; i < routinesNumber; i++ {
-		go func(i int) {
+		go func() {
 			for {
-				wq.routinesCond.L.Lock()
+				wq.waitMainSchedule()
 
-				wq.routinesLock.Lock()
-				wq.freeRoutines++
-				wq.routinesLock.Unlock()
+				work := wq.fetchWork()
 
-				wq.routinesCond.Wait()
-
-				wq.routinesLock.Lock()
-				wq.freeRoutines--
-				wq.routinesLock.Unlock()
-
-				wq.routinesCond.L.Unlock()
-
-				wq.listRWLock.Lock()
-				work := wq.workList[0]
-				wq.workList = wq.workList[1:]
-				wq.workNumbers--
-				wq.listRWLock.Unlock()
-
-				work.Action(work)
+				if work != nil {
+					work.Action(work)
+				}
 			}
-		}(i)
+		}()
 	}
 
 	return wq
